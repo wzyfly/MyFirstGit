5c777971cb0ee041b24ec2c4609f863be36056a0
wzy <baidu@baidutekiMacBook-Air.local>
2015-01-28 20:12:39 +0800

    add file

---
+++ Content/MyImage.h
@@ -0,0 +1,22 @@
+//
+//  MyImage.h
+//  testFramework
+//
+//  Created by wzy on 15/1/26.
+//  Copyright (c) 2015年 wzy. All rights reserved.
+//
+
+#ifndef testFramework_MyImage_h
+#define testFramework_MyImage_h
+#import <Foundation/Foundation.h>
+
+
+@interface MyImage : NSObject
+
+- (void)printLog:(id) log;
+
+@end
+
+
+
+#endif
---
+++ Content/MyImage.m
@@ -0,0 +1,17 @@
+//
+//  MyImage.m
+//  testFramework
+//
+//  Created by wzy on 15/1/26.
+//  Copyright (c) 2015年 wzy. All rights reserved.
+//
+
+#import "MyImage.h"
+@implementation  MyImage
+
+- (void)printLog:(id) log {
+    NSLog(@"%@", log);
+}
+
+@end
+
